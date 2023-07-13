d620686bf123aff44c54c97317b5de43b577c2b6
pustart <polaris9955@gmail.com>
2023-07-13 23:24:50 +0300

    feat: add settings form handler

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,32 @@ import { DEFAULT_BREAK_INTERVAL, DEFAULT_POMODORO, DEFAULT_LONG_BREAK, DEFAULT_S
 import Timer from "./timer.js";
 import Modal from './modal.js';
 
-const timer = new Timer(DEFAULT_POMODORO, DEFAULT_SHORT_BREAK, DEFAULT_LONG_BREAK, DEFAULT_BREAK_INTERVAL);
-window.timer = timer;
-
 const settingsModal = new Modal("settings-modal");
 
 document.querySelector('#open-settings-btn').addEventListener('click', () => {
   settingsModal.openModal();
 });
+
+const timer = new Timer(DEFAULT_POMODORO, DEFAULT_SHORT_BREAK, DEFAULT_LONG_BREAK, DEFAULT_BREAK_INTERVAL);
+window.timer = timer;
+
+document.querySelector('.settings-modal-form').addEventListener('submit', handleSettingsSubmit);
+
+function handleSettingsSubmit(event) {
+  event.preventDefault();
+
+  const pomodoroInput = document.getElementById("pomodoroInput");
+  const shortBreakInput = document.getElementById("shortBreakInput");
+  const longBreakInput = document.getElementById("longBreakInput");
+
+  const pomodoroValue = parseInt(pomodoroInput.value, 10);
+  const shortBreakValue = parseInt(shortBreakInput.value, 10);
+  const longBreakValue = parseInt(longBreakInput.value, 10);
+
+  timer.pomodoro = pomodoroValue;
+  timer.shortBreak = shortBreakValue;
+  timer.longBreak = longBreakValue;
+
+  timer.resetTimer();
+  settingsModal.closeModal();
+}
