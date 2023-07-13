dc6bb1be6e4a03b21d6dd20e23241fc3e840f667
pustart <polaris9955@gmail.com>
2023-07-13 22:53:49 +0300

    feat: add modal window

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 import { DEFAULT_BREAK_INTERVAL, DEFAULT_POMODORO, DEFAULT_LONG_BREAK, DEFAULT_SHORT_BREAK } from "./constants.js";
 import Timer from "./timer.js";
+import Modal from './modal.js';
 
 const timer = new Timer(DEFAULT_POMODORO, DEFAULT_SHORT_BREAK, DEFAULT_LONG_BREAK, DEFAULT_BREAK_INTERVAL);
 window.timer = timer;
+
+const settingsModal = new Modal("settings-modal");
+
+document.querySelector('#open-settings-btn').addEventListener('click', () => {
+  settingsModal.openModal();
+});
